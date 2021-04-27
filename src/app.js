ce8f0b3ba750aa8be25d4fe874cf9ade1ad1c9b7
NoAi-coder <80481550+NoAi-coder@users.noreply.github.com>
2021-04-27 20:58:06 +0200

    Added JS functions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,67 @@
+//Display current day & time
+function getCurrentTime() {
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
 
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let currentTime = document.querySelector(".current");
+  return (currentTime.innerHTML = `Now: ${day} ${hour}:${minutes}`);
+}
+
+getCurrentTime();
+
+//Dsiplay current weather info
+function showWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let cityElement = document.querySelector("h1");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  //weather icon - not working
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+//Search city function when clicking button
+function search(city) {
+  let apiKey = "27ddfc7325668fadfd863bab705e25e8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function submitSearch(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  search(searchInputElement.value);
+}
+
+let form = document.querySelector("#search_city");
+form.addEventListener("submit", submitSearch);
+
+search("Stockholm");

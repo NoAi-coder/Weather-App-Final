ad377d77cbfd54d5ce7249ce31cfbc0bb6008065
NoAi-coder <80481550+NoAi-coder@users.noreply.github.com>
2021-04-27 21:36:55 +0200

    Moved forecast from HTML to JS & Added displayForecast on JS

--- src/app.js
+++ src/app.js
@@ -26,6 +26,36 @@ function getCurrentTime() {
 
 getCurrentTime();
 
+//weekly forecast
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 //Dsiplay current weather info
 function showWeather(response) {
   let tempElement = document.querySelector("#temp");
@@ -65,3 +95,4 @@ let form = document.querySelector("#search_city");
 form.addEventListener("submit", submitSearch);
 
 search("Stockholm");
+displayForecast();
